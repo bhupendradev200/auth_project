e5a14e337b423ab33670970bdb95ce3b5be5fa78
Bhupendra <bhupendram4u@gmail.com>
2024-08-16 02:46:23 +0530

    adding Default port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
