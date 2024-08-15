27545fa0c09f5f1f3d059f93838902ea8a85bff6
Bhupendra <bhupendram4u@gmail.com>
2024-08-16 02:35:18 +0530

    adding config file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
